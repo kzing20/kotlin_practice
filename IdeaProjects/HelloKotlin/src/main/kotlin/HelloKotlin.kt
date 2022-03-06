d9ed4a7898cf91cbdd2221ec5dca5acd43c9caa2
jykim0925 <mslkd0925@gmail.com>
2022-03-06 22:03:40 +0900

    no message

---
+++ IdeaProjects/HelloKotlin/src/main/kotlin/HelloKotlin.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("HelloKotlin!")
+    val i: Int = 1255
+    val j: Int? = 1255
+    println(i == j)
+    println(i === j)
+}
---
+++ IdeaProjects/datatype/src/main/kotlin/datatype.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var username:String = "Kildong"
+    var count:Int = 3
+    username = "Dooly"
+    println("username: ${username}")
+}
